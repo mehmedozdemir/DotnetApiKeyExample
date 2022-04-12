556f3f7373676bc16191a7446722daa0e7b5e088
Mehmet Özdemir <mehmed.ozdemir@gmail.com>
2022-04-12 14:09:51 +0300

    Apikey codes wrote.

--- Attributes/ApiKeyAttribute.cs
+++ Attributes/ApiKeyAttribute.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System.Threading.Tasks;
 
@@ -6,9 +7,33 @@ namespace ApiKeyExample.Attributes
     [AttributeUsage(validOn: AttributeTargets.Class)]
     public class ApiKeyAttribute : Attribute, IAsyncActionFilter
     {
+        private const string APIKEY = "ApiKey";
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
+            if (!context.HttpContext.Request.Headers.TryGetValue(APIKEY, out var extractedApiKey))
+            {
+                context.Result = new ContentResult
+                {
+                    StatusCode = 401,
+                    Content = "Api key was not provided"
+                };
+                return;
+            }
 
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apikey = configuration.GetValue<string>("ApiKey");
+
+            if (!apikey.Equals(extractedApiKey))
+            {
+                context.Result = new ContentResult
+                {
+                    StatusCode = 401,
+                    Content = "Apikey is not valid"
+                };
+                return;
+            }
+
+            await next();
         }
     }
 }
